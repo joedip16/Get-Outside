93b160ea4f652f6666c1ab0df69251d0ae622570
Hoybaby <mbartek436@gmail.com>
2020-11-09 14:16:54 -0500

    finally got a functional api to work for us

--- javascript.js
+++ javascript.js
@@ -1,7 +1,24 @@
 var hello = "hello"
-var 
 
 // manoli is awesome
 
 var man = "woman"
 var women = "man"
+
+// "https://www.hikingproject.com/data/get-trails?lat=40.0583&lon=74.4057&maxDistance=10&key=200970119-76c616d7fe6494bc98bdcc7bc2b32137"
+
+
+function searchNationalPark(state) {
+
+    state = "nj";
+
+    $.ajax({
+        type: "GET",
+        url: `https://developer.nps.gov/api/v1/parks?stateCode=${state}&stateCode=&api_key=I7qeJmQumHGPtOSAaV4GqitG18NPnFk01uOCalSh`,
+        dataType: "json",
+    }).then(function(data) {
+        console.log(data);
+    })
+};
+
+searchNationalPark();
