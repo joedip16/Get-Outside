4e6e1249abbd6b9d7498b2b9ac77642b4eab6da7
Hoybaby <mbartek436@gmail.com>
2020-11-10 10:54:05 -0500

    attempting to apply weather onto page

--- javascript.js
+++ javascript.js
@@ -13,7 +13,7 @@ function searchNationalPark(state) {
     state = "nj";
 
     $.ajax({
-        type: "GET",
+       method: "GET",
         url: `https://developer.nps.gov/api/v1/parks?stateCode=${state}&stateCode=&api_key=I7qeJmQumHGPtOSAaV4GqitG18NPnFk01uOCalSh`,
         dataType: "json",
     }).then(function(data) {
@@ -22,3 +22,20 @@ function searchNationalPark(state) {
 };
 
 searchNationalPark();
+
+
+function fiveDayWeather (city) {
+
+    city = "Morristown";
+    // var apiKey= "e8bb5a6ba8f08e6e176eb848cae16ef7"
+
+    $.ajax({
+        method: "GET",
+        url: `api.openweathermap.org/data/2.5/forecast?q=${city}&appid=e8bb5a6ba8f08e6e176eb848cae16ef7`,
+        dataType: "json",
+    }).then(function(response){
+        console.log(response);
+    })
+}
+
+fiveDayWeather();
