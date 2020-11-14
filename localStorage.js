401a61879143483ee28a1c627e51cce7de60ab60
joedip16 <joedippolito16@gmail.com>
2020-11-14 11:44:55 -0500

    working on local storage

--- localStorage.js
+++ localStorage.js
@@ -2,12 +2,14 @@ $(document).ready(function() {
     $(".learn-more-btn").each(function () {
         //get id of the element and store it in a variable
         var learnMoreBtn = document.querySelector(".learn-more-btn")
+        
         //use the id to get the color from local storage and store it in a variable
-        var getColor = localStorage.getItem(".learn-more-btn")
+        var knowlton = localStorage.getItem(".learn-more-btn")
+        
         //update the background of the element to be the local storage color
         $(this).css("background-color", null);
 
-
+        
       });
     
       $(".learn-more-btn").on("click", function () { 
@@ -15,13 +17,20 @@ $(document).ready(function() {
         $(this).css("background-color", "yellow"); 
         
         
-        //get id of the element and store it in a variable
-        var city = $(this).attr("id");  
+        // //get id of the element and store it in a variable
+        var city = $(this).attr("id"); 
     
-        //use the id to add the color to local storage
+        // //use the id to add the color to local storage
         localStorage.setItem(city, "yellow"); 
+        // console.log(city)
       });
     });
+
+    var buttons = document.getElementsByClassName("learn-more-btn")
+    console.log("this is buttons", buttons)
+    for(var i=0; i < buttons.length; i++){
+        
+        }
     
 
 
@@ -61,29 +70,6 @@ $(document).ready(function() {
   
     
 //   });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // var learnMoreBtn = document.querySelector(".gap-btn")
 // var rememberBtn = localStorage.getItem("btn")
