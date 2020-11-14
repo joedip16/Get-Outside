33645536f75b4b16d3d0200f1271b5a77bb88a24
joedip16 <joedippolito16@gmail.com>
2020-11-14 12:36:50 -0500

    working on local storage

--- localStorage.js
+++ localStorage.js
@@ -1,37 +1,42 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $(".learn-more-btn").each(function () {
         //get id of the element and store it in a variable
         var learnMoreBtn = document.querySelector(".learn-more-btn")
-        
+
         //use the id to get the color from local storage and store it in a variable
         var knowlton = localStorage.getItem(".learn-more-btn")
-        
+
         //update the background of the element to be the local storage color
         $(this).css("background-color", null);
 
-        
-      });
-    
-      $(".learn-more-btn").on("click", function () { 
-        //set the background yellow 
-        $(this).css("background-color", "yellow"); 
-        
-        
-        // //get id of the element and store it in a variable
-        var city = $(this).attr("id"); 
-    
-        // //use the id to add the color to local storage
-        localStorage.setItem(city, "yellow"); 
-        // console.log(city)
-      });
+
     });
 
-    var buttons = document.getElementsByClassName("learn-more-btn")
-    console.log("this is buttons", buttons)
-    for(var i=0; i < buttons.length; i++){
-        
-        }
+    $(".learn-more-btn").on("click", function () {
+        //set the background yellow 
+        $(this).css("background-color", "yellow");
+
+
+        // //get id of the element and store it in a variable
+        var city = $(this).attr("id");
+
+        // //use the id to add the color to local storage
+        localStorage.setItem(city, "yellow");
+        // console.log(city)
+        var buttons = document.getElementsByClassName("learn-more-btn")
+// console.log("this is buttons", buttons)
+for (var i = 0; i < buttons.length; i++) {
+    console.log(buttons[i])
+    localStorage.setItem(city, "yellow");
+    $(this).css("background-color", "yellow");
     
+    }
+    });
+    // var city = $(this).attr("id"); 
+
+});
+
+
 
 
 
@@ -64,11 +69,11 @@ $(document).ready(function() {
     //   //   }
     //   });
 
-    
+
     // }
-  
-  
-    
+
+
+
 //   });
 
 // var learnMoreBtn = document.querySelector(".gap-btn")
